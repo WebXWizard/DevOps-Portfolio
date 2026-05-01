f8fdc3338ab00164b023d04cfe77b9a01e51b777
Shahe Alam <alamshahe9044@gmail.com>
2026-05-01 21:11:41 +0530

    🚀 Changes Done in Portfolio ✅

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/Image_Editor.png';
+import profileImage from '../../assets/Profile-Image.png';
 import resumePDF from '../../assets/Shahe Alam Resume -DevOps.pdf';
 
 const About = () => {
